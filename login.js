eb0016dc61b06bd98dad989ab316bdd72ccc4aa4
Sanjukstanly <93672458+Sanjukstanly@users.noreply.github.com>
2021-11-15 16:40:06 +0530

    c

--- login.js
+++ login.js
@@ -15,3 +15,29 @@
       }
   }
  
+  
+  function verifyPassword() {  
+    var pw = document.getElementById("pswd").value;  
+    //check empty password field  
+    if(pw == "") {  
+       document.getElementById("message").innerHTML = "**Fill the password please!";  
+       return false;  
+    }  
+     
+   //minimum password length validation  
+    if(pw.length < 8) {  
+       document.getElementById("message").innerHTML = "**Weak Password";  
+       return false;  
+    }  
+    
+  //maximum length of password validation  
+    if(pw.length > 15) {  
+       document.getElementById("message").innerHTML = "Shouldn't Be Greater Than 15";  
+       return false;  
+    } else {  
+       document.getElementById("message").innerHTML= "Password is correct"; 
+       document.getElementById("message").style.color="green";
+       return true; 
+    } 
+    
+  }  
